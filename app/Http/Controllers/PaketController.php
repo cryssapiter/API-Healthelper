4a092a5a25f2c55946134a68a5ff011550e81855
Faradilla Azranur <faradillaazra@gmail.com>
2020-11-18 21:38:37 +0700

    Paket CRUD to read only

--- app/Http/Controllers/PaketController.php
+++ app/Http/Controllers/PaketController.php
@@ -12,28 +12,4 @@ class PaketController extends Controller
         $data = PaketModel::where('id_paket',$id_paket)->get();
         return response ($data);
     }
-
-    //Fungsi Create
-    public function store(Request $request){
-        $data = new PaketModel();
-        $data->jenis_konsultasi = $request -> input('jenis_konsultasi');
-        $data->id_user = $request -> input('id_user');
-        $data->save();
-        return response('Data paket berhasil ditambahkan');
-    }
-
-    //Fungsi Update
-    public function update(Request $request, $id_paket){
-        $data = PaketModel::where('id_paket',$id_paket)->first();
-        $data->jenis_konsultasi = $request -> input('jenis_konsultasi');
-        $data->save();
-        return response('Data paket berhasil diupdate');
-    }
-
-    // Fungsi Delete
-    public function destroy($id_paket){
-        $data = PaketModel::where('id_paket',$id_paket)->first();
-        $data->delete();
-        return response('Data paket berhasil dihapus');
-    }
 }
